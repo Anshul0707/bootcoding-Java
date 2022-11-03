8fd3777bbb89be1f5fce402b6e64d6f66728a6a2
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-03 11:27:29 +0530

    Second Largest Number

---
+++ src/main/java/Exercise/Program4.java
@@ -0,0 +1,24 @@
+package Exercise;
+
+import java.util.Arrays;
+
+public class Program4
+{
+
+    public static void main(String[] args)
+    {
+            getSecondLargNum();
+
+    }
+
+    private static void getSecondLargNum()
+    {
+        int a[] = { 12, 35, 1, 10, 34, 1 };
+        int length = a.length;
+        System.out.println(length);
+        Arrays.sort(a);
+        System.out.println("Sorted Array is"+Arrays.toString(a));
+        int result = a[length - 2];
+        System.out.println("Second Largest Number is " +result);
+    }
+}
