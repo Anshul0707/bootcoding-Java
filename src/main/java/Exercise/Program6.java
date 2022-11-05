aa84ac96033acaeafeea0f27ba837d979522aa1f
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-05 14:36:05 +0530

    Reverse a Given String

---
+++ src/main/java/Exercise/Program6.java
@@ -0,0 +1,28 @@
+package Exercise;
+
+import java.util.Scanner;
+
+//    Reverse a Given String
+public class Program6
+{
+    public static void main(String[] args)
+    {
+        reverseString();
+    }
+
+    private static void reverseString()
+    {
+        System.out.println("Enter a String..");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String rev=" ";
+        String[] s =str.split(" ");
+        for (String word:s)
+        {
+            rev=word + " " + rev;
+        }
+        System.out.println("Reverse String is ="+rev);
+
+    }
+
+}
