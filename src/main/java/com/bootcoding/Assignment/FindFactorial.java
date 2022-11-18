c4eb142773759eafce3b2b4ef8d29be8754d344c
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-18 13:05:42 +0530

    Write a program to find the factorial value of a given number

---
+++ src/main/java/com/bootcoding/Assignment/FindFactorial.java
@@ -0,0 +1,27 @@
+package com.bootcoding.Assignment;
+
+public class FindFactorial
+{
+    public static void main(String[] args)
+    {
+    getFact(29);
+    }
+
+    public static void getFact(int num)
+    {
+        if (num<=0)
+        {
+            System.out.println("Number should be positive or greater than zero");
+        }
+        else
+        {
+            long fact = 1;
+            for (int i=1;i<=num;i++)
+            {
+                fact=fact*i;
+            }
+            System.out.println("Factorial of " + num + " is " + fact);
+        }
+
+    }
+}
