fd29125c3d9b3fd04ad73cd169cb56b78b0b84cd
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-18 15:43:05 +0530

    Write a program to print out all Armstrong numbers between 1 and 500.

---
+++ src/main/java/com/bootcoding/Assignment/ArmstrongNumber.java
@@ -0,0 +1,34 @@
+package com.bootcoding.Assignment;
+
+public class ArmstrongNumber
+{
+    public static void main(String[] args)
+    {
+
+        armstrongOrNot();
+    }
+
+    public static void armstrongOrNot()
+    {
+        for (int i=1;i<=500;i++)
+        {
+            int num = i;
+            int x;
+            int result=0;
+
+            while(num!=0)
+            {
+                x=num%10;
+                result=result+(x*x*x);
+                num=num/10;
+            }
+            if (i==result)
+            {
+                System.out.println(result);
+            }
+        }
+
+
+    }
+
+}
