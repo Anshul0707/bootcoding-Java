b1bb0c2729b3bf8d3062270d4e73f57d5fc86e20
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-18 14:47:14 +0530

    Write a program to print the sum of the even and odd integers.

---
+++ src/main/java/com/bootcoding/Assignment/SumOfEvenAndOdd.java
@@ -0,0 +1,35 @@
+package com.bootcoding.Assignment;
+
+public class SumOfEvenAndOdd
+{
+    public static void main(String[] args)
+    {
+       getSum();
+    }
+
+
+
+
+    private static void getSum()
+    {
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        int even = 0, odd = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0)
+            {
+                even=even+arr[i];
+            }
+            else
+            {
+                odd =odd+arr[i];
+            }
+
+        }
+
+        System.out.println("Sum of the Even " + even);
+        System.out.println("sum of the Odd " + odd);
+    }
+
+
+}
