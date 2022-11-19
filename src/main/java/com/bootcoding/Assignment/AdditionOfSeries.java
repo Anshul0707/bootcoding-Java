e130b8fa46df3bcf22ef02fa545a5c927a8be543
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-19 14:10:41 +0530

    Write a program to calculate the sum of following series where n is input by user. 1 + 1/2 + 1/3 + 1/4 + 1/5 +............1/n

---
+++ src/main/java/com/bootcoding/Assignment/AdditionOfSeries.java
@@ -0,0 +1,20 @@
+package com.bootcoding.Assignment;
+
+public class AdditionOfSeries
+{
+    public static void main(String[] args)
+    {
+        sum(5);
+    }
+
+    public static double sum(int num)
+    {
+        double add = 0.0;
+        for (double i=1;i<=num;i++)
+        {
+           add=add+1/i;
+        }
+        System.out.println(add);
+        return add;
+    }
+}
