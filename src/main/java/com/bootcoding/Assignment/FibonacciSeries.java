0bc4c7e0f29ca8b37676e534dbd09ef7f714f13f
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-19 14:11:53 +0530

    Write a program to print Fibonacci series of n terms where n is input by user : 0 1 1 2 3 5 8 13 24

---
+++ src/main/java/com/bootcoding/Assignment/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package com.bootcoding.Assignment;
+
+public class FibonacciSeries
+{
+    public static void main(String[] args)
+    {
+    printFibonacciSeries(10);
+    }
+
+    public static void printFibonacciSeries(int n)
+    {
+        int first = 0;
+        int second=1;
+        int last;
+
+        for (int i=2;i<=n;i++)
+        {
+            last=first+second;
+            first=second;
+            second=last;
+            System.out.println(last);
+        }
+    }
+}
