ddab0a5c99fec9c698d817efe3e5675ef6cbb3ee
Anshul0707 <anshul.markwade786@gmail.com>
2023-01-31 11:07:25 +0530

    ..

--- src/main/java/com/bootcoding/ConditionalsAndLogic/Quadratic.java
+++ src/main/java/com/bootcoding/ConditionalsAndLogic/Quadratic.java
@@ -9,15 +9,31 @@ public class Quadratic
         //if d > 0, roots are real and different
         //if d == 0, roots are real and equal
         //if d < 0, roots are complex complex and different
-    public static void findRoots(int a,int b,int c)
-    {
-    double d=(b*b)-4*a*c;
-    if (d>0)
-    {
-      double  root1 = (-b + Math.sqrt(d)) / (2 * a);
-       double root2 = (-b - Math.sqrt(d)) / (2 * a);
-        System.out.println(root1);
-        System.out.println(root2);
-    }
+    public static void findRoots(int a,int b,int c) {
+        double d = (b * b) - 4 * a * c;
+        double root1;
+        double root2;
+        if (d > 0)
+        {
+            root1 = (-b + Math.sqrt(d)) / (2 * a);
+            root2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println(root1);
+            System.out.println(root2);
+            System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
+        }
+        else if (d == 0)
+        {
+            root1 = root2 = -b / (2 * a);
+            System.out.format("root1 = root2 = %.2f;", root1);
+        }
+
+        else
+        {
+            double real = -b / (2 * a);
+            double imaginary = Math.sqrt(-d) / (2 * a);
+            System.out.format("root1 = %.2f+%.2fi", real, imaginary);
+            System.out.format("\nroot2 = %.2f-%.2fi", real, imaginary);
+        }
+
     }
 }
