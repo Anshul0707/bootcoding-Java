065653e75dc555613844d6f1df98e1d7c541eb2c
Anshul0707 <anshul.markwade786@gmail.com>
2023-02-02 12:36:43 +0530

    removing comments

--- src/main/java/com/bootcoding/arrayAssignment/PositiveNegativeArray.java
+++ src/main/java/com/bootcoding/arrayAssignment/PositiveNegativeArray.java
@@ -1,8 +1,4 @@
 package com.bootcoding.arrayAssignment;
-//WAP to print positive number if number is 3 digit and above and
-// print negative number if number is 2 digit only of given array
-//a = [10,-22, 23, 222]
-//o/p=-22, 222
 public class PositiveNegativeArray
 {
     public static void main(String[] args)
