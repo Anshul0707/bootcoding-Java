d5252019d06ece70fb86711556c0b304f49271c6
Anshul0707 <anshul.markwade786@gmail.com>
2023-02-17 14:42:22 +0530

    getToday method has been created.

--- src/main/java/com/bootcoding/exercism/BirdWatcher.java
+++ src/main/java/com/bootcoding/exercism/BirdWatcher.java
@@ -5,11 +5,16 @@ public class BirdWatcher
     public static void main(String[] args) {
         int[] birdsPerDay = { 2, 5, 0, 7, 4, 1 };
         getLastWeek(birdsPerDay);
+        getToday(birdsPerDay);
     }
     public static void getLastWeek(int[] birdsPerDay) {
         System.out.println("LastWeek");
         for (int res: birdsPerDay) {
             System.out.println(res);
         }
+    }
+    public static int getToday(int[] birdsPerDay){
+        int res=birdsPerDay[birdsPerDay.length-1];
+        return res;
     }
 }
