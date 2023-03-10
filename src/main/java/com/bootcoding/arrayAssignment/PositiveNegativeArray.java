7810bfc13cfd4823a01c11ea38140c2459e12874
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-10 10:17:32 +0530

    ..

--- src/main/java/com/bootcoding/arrayAssignment/PositiveNegativeArray.java
+++ src/main/java/com/bootcoding/arrayAssignment/PositiveNegativeArray.java
@@ -1,39 +1,29 @@
 package com.bootcoding.arrayAssignment;
 public class PositiveNegativeArray
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int [] aa={-10,-222, 23, 2222};
         positiveNegativeArray(aa);
     }
-    public static  void positiveNegativeArray(int [] aa)
-    {
-
-        for (int i = 0; i < aa.length; i++)
-        {
+    public static  void positiveNegativeArray(int [] aa) {
+        for (int i = 0; i < aa.length; i++){
             int count=0;
             int n=aa[i];
-            if (n>0)
-            {
-                while (n != 0)
-                {
+            if (n>0) {
+                while (n != 0) {
                     n = n / 10;
                     count++;
                 }
-                if (count>=3)
-                {
+                if (count>=3) {
                     System.out.println(aa[i]);
                 }
             }
-            else
-            {
-                while (n != 0)
-                {
+            else {
+                while (n != 0) {
                     n = n / 10;
                     count++;
                 }
-                if (count==2)
-                {
+                if (count==2) {
                     System.out.println(aa[i]);
                 }
             }
