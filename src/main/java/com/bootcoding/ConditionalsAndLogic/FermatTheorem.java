4758b6266f146870471746c7f6d27a024a1c5a67
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-15 17:31:13 +0530

    ..

--- src/main/java/com/bootcoding/ConditionalsAndLogic/FermatTheorem.java
+++ src/main/java/com/bootcoding/ConditionalsAndLogic/FermatTheorem.java
@@ -9,12 +9,6 @@ public class FermatTheorem
 
     public static void fermatTheorem(int a, int b, int c, int n)
     {
-//        int a=Math.pow(a,n);
-//        a2+b2=c2
-        // d = a2+b2
-        // e = c2
-        // d == e
-
         double d=Math.pow(a,n)+ Math.pow(b,n);
         double e=Math.pow(c,n);
         System.out.println(d);
