7eecb2ca58127c2aad47e6e9a95567b25d027408
Anshul0707 <anshul.markwade786@gmail.com>
2023-03-21 16:52:24 +0530

    basic changes

--- src/main/java/com/bootcoding/Assignment/CalculateSum.java
+++ src/main/java/com/bootcoding/Assignment/CalculateSum.java
@@ -2,13 +2,11 @@ package com.bootcoding.Assignment;
 
 public class CalculateSum
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         sum();
     }
 
-    private static void sum()
-    {
+    private static void sum() {
         int sum=1;
         for (int i=1;i<=10;i++) {
              sum=sum+i;
