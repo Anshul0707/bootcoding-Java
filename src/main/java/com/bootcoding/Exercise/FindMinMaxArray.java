fd836f497e42325f1985fca1222a3a6416da9e46
Anshul0707 <anshul.markwade786@gmail.com>
2024-02-05 16:47:51 +0530

    ..

--- src/main/java/com/bootcoding/Exercise/FindMinMaxArray.java
+++ src/main/java/com/bootcoding/Exercise/FindMinMaxArray.java
@@ -2,34 +2,27 @@ package com.bootcoding.Exercise;
 
 import java.util.Arrays;
 
-public class FindMinMaxArray
-{
-    static int[] a ={1,2,5,6,3,2,8}; //Given Number
+public class FindMinMaxArray {
+    static int[] a = {1, 2, 5, 6, 3, 2, 8}; //Given Number
 
-    private static void getMinNum()
-    {
-        int min=a[0];
-        for (int i=1;i< a.length;i++)
-        {
-            if (min>a[i])
-            {
-                min=a[i];
+    private static void getMinNum() {
+        int min = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min > a[i]) {
+                min = a[i];
             }
         }
-        System.out.println("Min value is :"+min);
+        System.out.println("Min value is :" + min);
     }
 
-    private static void getMaxNum()
-    {
-        int max=a[0];
-        for (int i=1;i< a.length;i++)
-        {
-            if (a[i]>max)
-            {
-                max=a[i];
+    private static void getMaxNum() {
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
             }
         }
-        System.out.println("max value is :"+max);
+        System.out.println("max value is :" + max);
     }
 
     public static void main(String[] args) {
